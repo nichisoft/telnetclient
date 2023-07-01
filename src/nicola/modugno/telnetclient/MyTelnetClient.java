7e3ea2afe4d745aa5e5d01c043fd69f691c7b305
Nicola Modugno <nichisoft@gmail.com>
2023-07-01 20:23:28 +0200

    Add files via upload

--- src/nicola/modugno/telnetclient/MyTelnetClient.java
+++ src/nicola/modugno/telnetclient/MyTelnetClient.java
@@ -1,9 +1,7 @@
 package nicola.modugno.telnetclient;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 import org.apache.commons.net.telnet.TelnetClient;
@@ -17,6 +15,17 @@ public class MyTelnetClient {
 			final int PORT=Integer.parseInt(args[1]);
 			MyTelnetClient myTelnetClient=new MyTelnetClient();
 			myTelnetClient.connect(IP, PORT);
+//			myTelnetClient.sendCommand("?RGC");
+//			myTelnetClient.sendCommand("?P");
+//			myTelnetClient.sendCommand("?F");
+//			myTelnetClient.sendCommand("?GAH");
+//			myTelnetClient.sendCommand("?GAP"); //restituisce la cartella corrente
+//			myTelnetClient.sendCommand("00001GHP");
+//			myTelnetClient.sendCommand("00005GHP");
+//			myTelnetClient.sendCommand("?ICA");
+//			myTelnetClient.sendCommand("?GIA0000100008");
+//			myTelnetClient.sendCommand("?GIA0000900011");
+			myTelnetClient.sendCommand("00001GHP");
 		}
 		else {
 			throw new IllegalArgumentException("IP PORT");
@@ -34,32 +43,49 @@ public class MyTelnetClient {
 	}
 	
 	public String sendCommand(final String command) {
-		String output=null;
+		StringBuffer output=null;
 		if(telnet!=null && telnet.isConnected()) {
 			InputStream in = telnet.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(telnet.getInputStream()));
+//			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 			
 			PrintStream out = new PrintStream(telnet.getOutputStream());
 			out.println(command);
 			out.flush();
 			
 			try {
-				output=reader.readLine();
-				System.out.println("COMMAND="+command+ " OUTPUT="+output);
+				
+				output = new StringBuffer();
+				byte[] buf = new byte[4096];
+				int len = 0;
+				Thread.sleep(750L);
+				while ((len = in.read(buf)) != 0) {
+					output.append(new String(buf, 0, len));
+					Thread.sleep(750L);
+			        if (in.available() == 0)
+			        	break;
+				}
+//				output=reader.readLine();
+//				output = reader.lines().collect(Collectors.joining("\r\n"));
+
+				System.out.println("COMMAND="+command);
+				System.out.println("OUTPUT=");
+				System.out.println(output.toString());
 			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			finally {
 				if(out!=null) {
 					out.close();
 				}
-				if(reader!=null) {
-					try {
-						reader.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
+//				if(reader!=null) {
+//					try {
+//						reader.close();
+//					} catch (IOException e) {
+//						e.printStackTrace();
+//					}
+//				}
 				if(in!=null) {
 					try {
 						in.close();
@@ -69,7 +95,7 @@ public class MyTelnetClient {
 				}
 			}
 		}
-		return output;
+		return output.toString();
 	}
 	
 	public void disconnect() {
